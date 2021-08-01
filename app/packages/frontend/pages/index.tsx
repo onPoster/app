aebaa241c2a337147e437031cecfa38bdc80ffed
jjperezaguinaga <me@jjperezaguinaga.com>
2021-08-01 00:13:37 +0000

    hotfix: Fixing broken instructions for deployment

--- app/packages/frontend/pages/index.tsx
+++ app/packages/frontend/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider, Heading, Input, Text } from '@chakra-ui/react'
+import { Box, Button, Heading, Input, Text } from '@chakra-ui/react'
 import { ChainId, useEthers, useSendTransaction } from '@usedapp/core'
 import { ethers, providers, utils } from 'ethers'
 import React, { useReducer } from 'react'
