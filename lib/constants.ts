45fe40153bf0525b0fd8890540be96a3265ada27
jjperezaguinaga <me@jjperezaguinaga.com>
2021-08-07 10:13:14 +0000

    chore: Fixes #17 - Added version and assets for branding

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const POSTER_SUBGRAPH_ID = 'jjperezaguinaga/poster'
+export const POSTER_SUBGRAPH_URL = `https://api.thegraph.com/subgraphs/name/${POSTER_SUBGRAPH_ID}`
+
+// @TODO Fetch these values directly from package
+export const POSTER_CONTRACT_VERSION = 'v6'
+export const POSTER_APP_VERSION = '0.1.0'
