c1bd277436aaf5562044f3bd1be50636d94ddcb1
jjperezaguinaga <me@jjperezaguinaga.com>
2021-10-10 18:26:47 +0000

    feat: Fixes #70 - Switched from Infura to Pokt Network

--- constants/ethereum.ts
+++ constants/ethereum.ts
@@ -5,7 +5,7 @@ export const DEFAULT_IPFS_GATEWAY = 'https://ipfs.infura.io/ipfs/'
 
 export const ETHEREUM_PROVIDERS = {
   [ChainId.Goerli]: `https://goerli.infura.io/v3/${INFURA_ID}`,
-  [ChainId.Polygon]: `https://polygon-mainnet.infura.io/v3/${INFURA_ID}`,
+  [ChainId.Polygon]: `https://poly-mainnet.gateway.pokt.network/v1/lb/61632ea06019490034d569a2`,
   [ChainId.Hardhat]: 'http://localhost:8555',
   [ChainId.Localhost]: 'http://localhost:8545',
 }
